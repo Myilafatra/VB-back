4b83596a35da67e33bebe1010e4c15be4587ddce
Myilafatra <miarisoa_68@sayna.io>
2019-10-09 20:45:55 +1000

    premier push

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: "mongodb://localhost:27017/bibliotheque",
+  //mongoURI: "mongodb+srv://test:test@cluster0-dg2ka.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
